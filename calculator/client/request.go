a11acd536efacb9af68e58c4822d86aa8daf6f84
RichardRivaldo <richardrivaldo84@gmail.com>
2022-04-30 16:32:28 +0700

    updated request and services for prime

--- calculator/client/request.go
+++ calculator/client/request.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	pb "github.com/RichardRivaldo/grpc-go/calculator/proto"
@@ -20,4 +21,29 @@ func add(client pb.CalculatorServiceClient) {
 	}
 
 	log.Println("Sum Result:", res.Sum)
+}
+
+func prime(client pb.CalculatorServiceClient) {
+	log.Println("Prime Service Invoked!")
+
+	stream, err := client.Prime(context.Background(), &pb.PrimeRequest{
+		Number: 210,
+	})
+
+	if err != nil {
+		log.Fatalf("Error when streaming prime, reason %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error when streaming prime, reason %v\n", err)
+		}
+
+		log.Println(msg.Prime)
+	}
 }
--- calculator/server/services.go
+++ calculator/server/services.go
@@ -8,9 +8,29 @@ import (
 )
 
 func (s *Server) Add(ctx context.Context, req *pb.SumRequest) (*pb.SumResponse, error) {
-	log.Printf("Greet with request of %v\n", req)
+	log.Printf("Unary - Sum with request of %v\n", req)
 
 	return &pb.SumResponse{
 		Sum: req.FirstNumber + req.SecondNumber,
 	}, nil
+}
+
+func (s *Server) Prime(req *pb.PrimeRequest, stream pb.CalculatorService_PrimeServer) error {
+	log.Printf("Stream - Prime request of %v\n", req)
+
+	factor := 2
+	current := req.Number
+
+	for current > 1 {
+		if current%int32(factor) == 0 {
+			stream.Send(&pb.PrimeResponse{
+				Prime: int32(factor),
+			})
+			current = current / int32(factor)
+		} else {
+			factor += 1
+		}
+	}
+
+	return nil
 }
