cae2b9c2a51c8afeeed67d194455c2942aef18b2
RichardRivaldo <richardrivaldo84@gmail.com>
2022-04-30 17:23:33 +0700

    added average in request and services

--- calculator/client/request.go
+++ calculator/client/request.go
@@ -47,3 +47,33 @@ func prime(client pb.CalculatorServiceClient) {
 		log.Println(msg.Prime)
 	}
 }
+
+func average(client pb.CalculatorServiceClient) {
+	log.Println("Prime Service Invoked!")
+
+	requests := []*pb.AverageRequest{
+		{Number: 1},
+		{Number: 3},
+		{Number: 2},
+		{Number: 4},
+	}
+
+	stream, err := client.Average(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error when streaming average, reason %v\n", err)
+	}
+
+	for i, req := range requests {
+		log.Printf("Sending requests of %d: %v\n", i, req)
+		stream.Send(req)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error when receiving result, reason %v\n", err)
+	}
+
+	log.Println(res.Average)
+}
--- calculator/server/services.go
+++ calculator/server/services.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	pb "github.com/RichardRivaldo/grpc-go/calculator/proto"
@@ -16,7 +17,7 @@ func (s *Server) Add(ctx context.Context, req *pb.SumRequest) (*pb.SumResponse, 
 }
 
 func (s *Server) Prime(req *pb.PrimeRequest, stream pb.CalculatorService_PrimeServer) error {
-	log.Printf("Stream - Prime request of %v\n", req)
+	log.Printf("Server Stream - Prime request of %v\n", req)
 
 	factor := 2
 	current := req.Number
@@ -33,4 +34,28 @@ func (s *Server) Prime(req *pb.PrimeRequest, stream pb.CalculatorService_PrimeSe
 	}
 
 	return nil
+}
+
+func (s *Server) Average(stream pb.CalculatorService_AverageServer) error {
+	log.Printf("Client Stream - Average request")
+
+	sum := 0
+	n := 0
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.AverageResponse{
+				Average: float32(sum) / float32(n),
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Failed receiving stream, reason %v\n", err)
+		}
+
+		sum += int(msg.Number)
+		n++
+	}
 }
